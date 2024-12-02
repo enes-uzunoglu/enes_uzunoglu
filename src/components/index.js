429d04d17c73338da9e2929d5ea344cb80ecc324
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2024-12-02 16:11:05 +0300

    Create necessary folder and file
    in components folder
    1- Creates component files
    2- Creates css files
    3- Creates index.js file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Hero } from './Hero/Hero';
+export { default as Footer } from './Footer/Footer';
+export { default as Profile } from './Profile/Profile';
+export { default as Projects } from './Projects/Projects';
+export { default as Skills } from './Skills/Skills';
